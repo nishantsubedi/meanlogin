8c4189dd36bc38b3857242f18b5607beff8db248
Nishant <nishantsubedi1996@gmail.com>
2018-08-22 19:40:26 +0545

    mongoose model and register users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // register route
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg: 'Failed to register user', err:err});
+        } else {
+            res.json({success: true, msg: 'User Registered'});
+        }
+    });
+
 });
 
 // authenticate route
@@ -16,9 +34,6 @@ router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
 });
 
-// validate route
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE');
-});
+
 
 module.exports = router;
