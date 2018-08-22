d12127ea05798d6c7391aeb809db38ab454d2213
Nishant <nishantsubedi1996@gmail.com>
2018-08-22 18:58:13 +0545

    express and mongoose setup

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// register route
+router.get('/register', (req, res, next) => {
+    res.send('REGISTER');
+});
+
+// authenticate route
+router.get('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATE');
+});
+
+// profile route
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+});
+
+// validate route
+router.get('/validate', (req, res, next) => {
+    res.send('VALIDATE');
+});
+
+module.exports = router;
